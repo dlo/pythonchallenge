4e3396c1e4a8d9bf5a063ea448d47e25756a3832
Dan Loewenherz <dloewenherz@gmail.com>
2009-11-29 17:05:50 -0800

    complete #5

---
+++ 5.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import re
+import httplib
+import pickle
+
+url = "http://www.pythonchallenge.com/pc/def/banner.p"
+
+conn = httplib.HTTPConnection('www.pythonchallenge.com')
+conn.request("GET", '/pc/def/banner.p')
+
+p = pickle.loads(conn.getresponse().read())
+
+for l in p:
+  line = ""
+  for item in l:
+    line = line + item[0]*item[1]
+  print line
+
